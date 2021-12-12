8fbc0d02868a446361bddc235667310a2f679653
dhockenb <42590475+dhockenb@users.noreply.github.com>
2021-12-11 18:25:38 -0800

    Add files via upload
    updated filepaths

--- app.py
+++ app.py
@@ -3,17 +3,17 @@ from pathlib import Path
 import fire
 
 
-from qualifier.utils.fileio import load_csv, save_csv
+from utils.fileio import load_csv, save_csv
 
-from qualifier.utils.calculators import (
+from utils.calculators import (
     calculate_monthly_debt_ratio,
     calculate_loan_to_value_ratio
 )
 
-from qualifier.filters.max_loan import filter_max_loan_size
-from qualifier.filters.credit_score import filter_credit_score
-from qualifier.filters.debt_to_income import filter_debt_to_income
-from qualifier.filters.loan_to_value import filter_loan_to_value
+from filters.max_loan import filter_max_loan_size
+from filters.credit_score import filter_credit_score
+from filters.debt_to_income import filter_debt_to_income
+from filters.loan_to_value import filter_loan_to_value
 
 
 def load_bank_data():
@@ -99,7 +99,6 @@ def run():
     qualifying_loans = find_qualifying_loans(
         bank_data, credit_score, debt, income, loan_amount, home_value
     )
-
 
 
 
